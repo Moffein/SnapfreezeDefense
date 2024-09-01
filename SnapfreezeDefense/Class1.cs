528b4dae83499648da463de2f702b91c733a1500
Moffein <22742925+Moffein@users.noreply.github.com>
2024-09-01 15:51:00 -0700

    Add project files.

---
+++ SnapfreezeDefense/Class1.cs
@@ -0,0 +1,64 @@
+﻿using BepInEx;
+using R2API;
+using R2API.Utils;
+using RoR2;
+using RoR2.Projectile;
+using SnapfreezeDefense.Components;
+using System;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace SnapfreezeDefense
+{
+    [BepInDependency("com.bepis.r2api", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInPlugin("com.Moffein.SnapfreezeDefense", "SnapfreezeDefense", "1.0.1")]
+    [R2API.Utils.R2APISubmoduleDependency(nameof(PrefabAPI), nameof(ContentAddition))]
+    [NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.EveryoneNeedSameModVersion)]
+    public class SnapfreezeDefense : BaseUnityPlugin
+    {
+        public static GameObject modifiedIceWallPillarProjectile;
+        public static GameObject modifiedIceWallWalkerProjectile;
+
+        public void Awake()
+        {
+            BuildProjectile();
+        }
+
+        private void BuildProjectile()
+        {
+            modifiedIceWallPillarProjectile = Addressables.LoadAssetAsync<GameObject>("RoR2/Base/Mage/MageIcewallPillarProjectile.prefab").WaitForCompletion().InstantiateClone("StandaloneRiskyModIceWallPillarProjectile", true);
+            modifiedIceWallPillarProjectile.AddComponent<IceWallDefenseComponent>();
+            ContentAddition.AddProjectile(modifiedIceWallPillarProjectile);
+
+            modifiedIceWallWalkerProjectile = Addressables.LoadAssetAsync<GameObject>("RoR2/Base/Mage/MageIcewallWalkerProjectile.prefab").WaitForCompletion().InstantiateClone("StandaloneRiskyModIceWallWalkerProjectile", true);
+            ProjectileMageFirewallWalkerController walker = modifiedIceWallWalkerProjectile.GetComponent<ProjectileMageFirewallWalkerController>();
+            if (walker) walker.firePillarPrefab = modifiedIceWallPillarProjectile;
+            ContentAddition.AddProjectile(modifiedIceWallWalkerProjectile);
+
+            SetEntityStateField("EntityStates.Mage.Weapon.PrepWall", "projectilePrefab", modifiedIceWallWalkerProjectile);
+
+            //Build deletion effect
+            GameObject deletionEffect = Addressables.LoadAssetAsync<GameObject>("RoR2/Base/Common/VFX/OmniImpactVFXFrozen.prefab").WaitForCompletion().InstantiateClone("StandaloneRiskyModIceWallDeletionEffect", false);
+            EffectComponent ec = deletionEffect.GetComponent<EffectComponent>();
+            ec.soundName = "Play_captain_drone_zap";
+
+            ContentAddition.AddEffect(deletionEffect);
+            IceWallDefenseComponent.projectileDeletionEffectPrefab = deletionEffect;
+        }
+
+        private bool SetEntityStateField(string entityStateName, string fieldName, UnityEngine.Object newObject)
+        {
+            EntityStateConfiguration esc = LegacyResourcesAPI.Load<EntityStateConfiguration>("entitystateconfigurations/" + entityStateName);
+            for (int i = 0; i < esc.serializedFieldsCollection.serializedFields.Length; i++)
+            {
+                if (esc.serializedFieldsCollection.serializedFields[i].fieldName == fieldName)
+                {
+                    esc.serializedFieldsCollection.serializedFields[i].fieldValue.objectValue = newObject;
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ SnapfreezeDefense/Components/IceWallDefenseComponent.cs
@@ -0,0 +1,106 @@
+﻿using RoR2;
+using RoR2.Projectile;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace SnapfreezeDefense.Components
+{
+    public class IceWallDefenseComponent : MonoBehaviour
+    {
+        public static GameObject projectileDeletionEffectPrefab;
+
+        private GameObject matrixObject;
+        private DefenseMatrixManager.DefenseMatrixManager.DefenseMatrixInfo defenseMatrixInfo;
+        private BoxCollider collider;
+        private TeamFilter tf;
+
+        //Do this in Start because TeamFilter might not be initialized in Awake
+        public void Start()
+        {
+            if (!matrixObject)
+            {
+                tf = base.GetComponent<TeamFilter>();
+                if (tf && tf.teamIndex != TeamIndex.None && base.transform)
+                {
+                    GameObject toInstantiate = new GameObject
+                    {
+                        layer = LayerIndex.entityPrecise.intVal
+                    };
+                    BoxCollider bc = toInstantiate.AddComponent<BoxCollider>();
+                    bc.size = new Vector3(2.5f, 2.5f, 7f);
+                    bc.enabled = false;
+
+                    toInstantiate.transform.localPosition = base.transform.localPosition;
+                    toInstantiate.transform.localRotation = base.transform.localRotation;
+                    toInstantiate.transform.localScale = base.transform.localScale;
+
+                    matrixObject = toInstantiate;
+                    collider = matrixObject.GetComponent<BoxCollider>();
+                    if (collider)
+                    {
+                        defenseMatrixInfo = new DefenseMatrixManager.DefenseMatrixManager.DefenseMatrixInfo(new Collider[] { collider }, tf.teamIndex);
+                        DefenseMatrixManager.DefenseMatrixManager.AddMatrix(defenseMatrixInfo);
+                    }
+                }
+            }
+        }
+
+        //This will handle the projectile deletion part
+        public void FixedUpdate()
+        {
+            if (!NetworkServer.active) return;
+            if (!tf || !collider) return;
+            DeleteProjectile();
+        }
+
+        public void DeleteProjectile()
+        {
+            List<ProjectileController> deletionList = new List<ProjectileController>();
+
+            Vector3 sizeVector = collider.size;
+
+            Collider[] colliders = Physics.OverlapBox(collider.transform.position, sizeVector, collider.transform.rotation, LayerIndex.projectile.mask);
+            foreach (Collider c in colliders)
+            {
+                ProjectileController pc = c.GetComponentInParent<ProjectileController>();
+                if (pc && !pc.cannotBeDeleted)
+                {
+                    if (!(pc.teamFilter && pc.teamFilter.teamIndex == tf.teamIndex))
+                    {
+                        bool cannotDelete = false;
+                        ProjectileSimple ps = pc.gameObject.GetComponent<ProjectileSimple>();
+                        ProjectileCharacterController pcc = pc.gameObject.GetComponent<ProjectileCharacterController>();
+
+                        if ((!ps || (ps && ps.desiredForwardSpeed == 0f)) && !pcc)
+                        {
+                            cannotDelete = true;
+                        }
+
+                        if (!cannotDelete)
+                        {
+                            deletionList.Add(pc);
+                        }
+                    }
+                }
+            }
+
+            int projectilesDeleted = deletionList.Count;
+            for (int i = 0; i < projectilesDeleted; i++)
+            {
+                GameObject toDelete = deletionList[i].gameObject;
+                if (toDelete)
+                {
+                    if (toDelete.transform && projectileDeletionEffectPrefab) EffectManager.SimpleEffect(projectileDeletionEffectPrefab, toDelete.transform.position, default, true);
+                    Destroy(toDelete);
+                }
+            }
+        }
+
+        public void OnDestroy()
+        {
+            if (defenseMatrixInfo != null) DefenseMatrixManager.DefenseMatrixManager.RemoveMatrix(defenseMatrixInfo);
+            if (matrixObject) Destroy(matrixObject);
+        }
+    }
+}
